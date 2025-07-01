89edb72a7df2099d3625a641283090aaccf3e3c5
Maria Luisa W. Cattani <m.lucattweb@gmail.com>
2025-07-01 17:22:53 -0300

    errr

--- app/utils/model.py
+++ app/utils/model.py
@@ -1,8 +1,10 @@
 # app/utils/model.py
 import joblib
 
+
 def load_model():
     return joblib.load("model/resume_model.pkl")
+
 
 def load_vectorizer():
     return joblib.load("model/tfidf_vectorizer.pkl")
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from app.routes import main
+from app.routes.main import main as main_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(main)
+
+
+app.register_blueprint(main_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
