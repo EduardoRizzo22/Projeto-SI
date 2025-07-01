e484ccf753b6d84522bec8a382c91ff78d057771
Maria Luisa W. Cattani <m.lucattweb@gmail.com>
2025-07-01 18:15:59 -0300

    mudar vectorizer

--- app/utils/nlp_processing.py
+++ app/utils/nlp_processing.py
@@ -3,8 +3,10 @@ import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 
+# Carrega lista de stopwords
 STOPWORDS = set(open("data/stopwords.txt").read().splitlines())
 
+# Função de pré-processamento
 def preprocess_text(text):
     text = text.lower()
     text = re.sub(r"\d+", "", text)
@@ -13,3 +15,9 @@ def preprocess_text(text):
     tokens = [t for t in tokens if t not in STOPWORDS and len(t) > 2]
     return " ".join(tokens)
 
+# Criação do vetorizar (não treinado ainda)
+vectorizer = TfidfVectorizer(
+    preprocessor=preprocess_text,
+    stop_words=STOPWORDS,
+    max_features=5000
+)
--- app/utils/pdf_parser.py
+++ app/utils/pdf_parser.py
@@ -1,7 +1,8 @@
 import fitz  # PyMuPDF
 
+
 def extract_text_from_pdf(file):
-    pdf = fitz.open(stream=file.read(), filetype="pdf")
+    pdf = fitz.open(stream=file.read(), filetype="txt")
     text = ""
     for page in pdf:
         text += page.get_text()
